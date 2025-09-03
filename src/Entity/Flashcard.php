d985df4990d7ac2bbddf96c6cbbd22f6132ece80
tombruairefrei <tom.bruaire@efrei.net>
2025-09-03 09:42:04 +0200

    Création d'une Entité Flashcard

---
+++ src/Entity/Flashcard.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FlashcardRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FlashcardRepository::class)]
+class Flashcard
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $question = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $reponse = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuestion(): ?string
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(string $question): static
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    public function getReponse(): ?string
+    {
+        return $this->reponse;
+    }
+
+    public function setReponse(string $reponse): static
+    {
+        $this->reponse = $reponse;
+
+        return $this;
+    }
+}
