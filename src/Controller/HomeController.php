334ae48ef00610643125b7e5efb6b5cb7bf87574
tombruairefrei <tom.bruaire@efrei.net>
2025-09-03 10:39:05 +0200

    Ajout du traitement pour ajouter une Flashcard dans la bdd

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $flashcard = new Flashcard();
 
@@ -20,8 +21,9 @@ final class HomeController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $flashcard = $form->getData();
-            //
+            // Ajout de la Flashcard dans la base de données
+            $entityManager->persist($flashcard);;
+            $entityManager->flush();
         }
 
         return $this->render('home/index.html.twig', [
